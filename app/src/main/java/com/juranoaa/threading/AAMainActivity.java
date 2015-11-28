b4bb955f2e65f215acaa1266c4c9766362af025b
Taehee Kim <slhyvaa@nate.com>
2015-11-28 22:32:30 +0900

    first commit

---
+++ app/src/main/java/com/juranoaa/threading/AAMainActivity.java
@@ -0,0 +1,28 @@
+package com.juranoaa.threading;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.juranoaa.threading.background.AABackgroundSampleActivity_;
+
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class AAMainActivity extends Activity {
+    private Intent intent;
+
+    @Click(R.id.btn_aa_background_sample_activity_call)
+    void btnAABackgroundSampleActivityCallClicked() {
+        intent = new Intent(this, AABackgroundSampleActivity_.class);
+        startActivity(intent);
+    }
+
+    @Click
+    void btnBackgroundSampleActivityCallClicked() {
+//        intent = new Intent(this, BackgroundSampleActivity.class);
+//        startActivity(intent);
+    }
+
+
+}
---
+++ app/src/main/java/com/juranoaa/threading/background/AABackgroundSampleActivity.java
@@ -0,0 +1,14 @@
+package com.juranoaa.threading.background;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.juranoaa.threading.R;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_background_sample)
+public class AABackgroundSampleActivity extends Activity {
+
+
+}
